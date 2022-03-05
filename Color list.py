ff6fc9b2ece70a3b602f2cbd07ea0b3287e0a7fd
Abhirami p kumar_kinghuzzzz <97221313+AbhiramiP123@users.noreply.github.com>
2022-03-05 23:24:51 +0530

    Delete Color list.py

--- Color list.py
+++
@@ -1,7 +0,0 @@
-color_list_1 = set(["Red", "Blue", "Green"])
-color_list_2 = set(["White", "Black"])
-print("Original set elements:")
-print("\nColor List 1:", color_list_1)
-print("\nColor List 2:", color_list_2)
-print("\nAll colors in Color list 1 but not in Color list 2:")
-print(color_list_1.difference(color_list_2))
